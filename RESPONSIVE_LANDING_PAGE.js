6771620b72c7ae3715034cbc502bc3fc8900ca25
Ankitan Kumar <ankitan4u@gmail.com>
2024-10-19 20:38:57 +0530

    Create RESPONSIVE_LANDING_PAGE.js

---
+++ RESPONSIVE_LANDING_PAGE.js
@@ -0,0 +1,9 @@
+// Navbar change on scroll
+window.addEventListener('scroll', function() {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('nav-scrolled');
+    } else {
+        navbar.classList.remove('nav-scrolled');
+    }
+});
